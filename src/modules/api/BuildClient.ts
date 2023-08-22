4fb5ef5017461c3a454758ac28fcdfe2b83d99da
nvalkovich <anastasiya.valkovich@mail.ru>
2023-08-22 21:04:38 +0300

    feat: add buildClient

--- src/modules/api/BuildClient.ts
+++ src/modules/api/BuildClient.ts
@@ -1,44 +1,22 @@
 import {
   ClientBuilder,
   // Import middlewares
-  type AuthMiddlewareOptions, // Required for auth
   type HttpMiddlewareOptions,
   PasswordAuthMiddlewareOptions,
-  Client, // Required for sending HTTP requests
+  Client,
+  TokenCache,
 } from '@commercetools/sdk-client-v2';
 
 const projectKey = process.env.CTP_PROJECT_KEY as string;
 const scopes = process.env.CTP_SCOPES?.split(' ');
-const CTP_AUTH_URL = process.env.CTP_AUTH_URL as string;
-const CTP_API_URL = process.env.CTP_API_URL as string;
-
-// Configure authMiddlewareOptions
-const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: CTP_AUTH_URL,
-  projectKey,
-  credentials: {
-    clientId: process.env.CTP_CLIENT_ID as string,
-    clientSecret: process.env.CTP_CLIENT_SECRET as string,
-  },
-  scopes,
-  fetch,
-};
 
 // Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: CTP_API_URL,
+  host: process.env.CTP_API_URL as string,
   fetch,
 };
 
-// Export the ClientBuilder
-export const ctpClient = new ClientBuilder()
-  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
-  .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware() // Include middleware for logging
-  .build();
-
-export const buildClientWithPasswordFlow = (options: PasswordAuthMiddlewareOptions): Client => {
+const buildClientWithPasswordFlow = (options: PasswordAuthMiddlewareOptions): Client => {
   const client = new ClientBuilder()
     .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
     .withPasswordFlow(options)
@@ -47,3 +25,5 @@ export const buildClientWithPasswordFlow = (options: PasswordAuthMiddlewareOptio
 
   return client;
 };
+
+export default buildClientWithPasswordFlow;
