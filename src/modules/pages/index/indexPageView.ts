09c78f50048ae52b7c8dc2cae1191a5588c81ae2
iamnkt <sg23mj@gmail.com>
2023-08-22 13:03:50 +0300

    refactor: change indexpageview content

--- src/modules/pages/index/indexPageView.ts
+++ src/modules/pages/index/indexPageView.ts
@@ -1,13 +1,48 @@
 import PageView from '../../core/pageView';
 
+interface MainData {
+  title: string;
+  content: string;
+}
+
 class IndexView extends PageView {
-  constructor(element: { title: string; content: string }) {
+  private title: string;
+
+  private content: string;
+
+  constructor(mainData: MainData) {
     super();
-    this.container.textContent = 'INDEX PAGE';
+    this.title = mainData.title;
+    this.content = mainData.content;
+  }
+
+  private insertMainInDOM(): void {
+    const footer = document.querySelector('footer');
+    if (footer) {
+      footer.insertAdjacentElement('beforebegin', this.container);
+    } else {
+      document.body.appendChild(this.container);
+    }
+  }
+
+  public clearContent(): void {
+    this.container.innerHTML = '';
   }
 
   public render(): HTMLElement {
-    return this.container;
+    const contentContainer = document.createElement('div');
+    contentContainer.innerHTML = `
+      <h1 class="visually-hidden">${this.title}</h1>
+      ${this.content}
+    `;
+    return contentContainer;
+  }
+
+  public setContent(content: PageView): void {
+    const contentContainer = document.createElement('div');
+    contentContainer.append(content.render());
+    this.container.appendChild(contentContainer);
+    this.insertMainInDOM();
   }
 }
 
