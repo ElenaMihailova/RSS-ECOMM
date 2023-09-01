07f7fb08e9f86cd043d5e856a845a80b6951211a
iamnkt <sg23mj@gmail.com>
2023-09-01 23:10:16 +0300

    feat: add catalog page controller

---
+++ src/modules/pages/catalog/catalogPageController.ts
@@ -0,0 +1,118 @@
+import { Product, ProductProjection } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/product';
+import {
+  filterProductsByOrigin,
+  getCategoryId,
+  getProductByCategory,
+  getProducts,
+  searchProducts,
+  sortProductsByNameAsc,
+  sortProductsByNameDesc,
+  sortProductsByPriceAsc,
+} from '../../api/apiClient';
+import { createElement, getElement } from '../../helpers/functions';
+import Router from '../../router/router';
+
+class CatalogController {
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+    // this.viewProducts();
+    // this.getCat('Black teas');
+    // this.getProductsByCat('Black teas');
+    this.filterByOrigin('China');
+    // this.sortProductsByName();
+    // this.sortProductsByPrice();
+    // this.searchProds('ceylon');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private async viewProducts(): Promise<any> {
+    const catalog: HTMLElement = getElement('.catalog__container');
+    const products = await getProducts();
+    console.log(products);
+    if (Array.isArray(products)) {
+      products.forEach((product: Product) => {
+        const productContainer = createElement({
+          tagName: 'div',
+          classNames: [`product-${product.key}`],
+          parent: catalog,
+        });
+
+        createElement({
+          tagName: 'img',
+          classNames: [`product-${product.key}__image`],
+          // attributes: [{src: `${product.masterData.current.masterVariant.images[0]}`}],
+          parent: productContainer,
+        });
+
+        createElement({
+          tagName: 'p',
+          classNames: [`product-${product.key}__title`],
+          // text: product.masterData.current.metaTitle,
+          parent: productContainer,
+        });
+
+        createElement({
+          tagName: 'p',
+          classNames: [`product-${product.key}__description`],
+          // text: product.masterData.current.metaDescription,
+          parent: productContainer,
+        });
+
+        const productPriceContainer = createElement({
+          tagName: 'div',
+          classNames: [`product-${product.key}__price-container`],
+          parent: productContainer,
+        });
+
+        createElement({
+          tagName: 'p',
+          classNames: [`product-${product.key}__price`],
+          // text: product.masterData.current.masterVariant.price,
+          parent: productPriceContainer,
+        });
+
+        createElement({
+          tagName: 'img',
+          classNames: [`product-${product.key}__weight`],
+          // text: ,
+          parent: productPriceContainer,
+        });
+      });
+    }
+  }
+
+  private async getCat(name: string): Promise<string | object> {
+    const categoryId = await getCategoryId(name);
+    return categoryId;
+  }
+
+  private async getProductsByCat(name: string): Promise<ProductProjection[] | object> {
+    const categoryId = await getCategoryId(name);
+    const products = await getProductByCategory(categoryId);
+    return products;
+  }
+
+  private async filterByOrigin(country: string): Promise<ProductProjection[] | object> {
+    const products = await filterProductsByOrigin(country);
+    return products;
+  }
+
+  private async sortProductsByName(): Promise<ProductProjection[] | object> {
+    const products = await sortProductsByNameDesc();
+    return products;
+  }
+
+  private async sortProductsByPrice(): Promise<ProductProjection[] | object> {
+    const products = await sortProductsByPriceAsc();
+    return products;
+  }
+
+  private async searchProds(str: string): Promise<ProductProjection[] | object> {
+    const products = await searchProducts(str);
+    return products;
+  }
+}
+
+export default CatalogController;
