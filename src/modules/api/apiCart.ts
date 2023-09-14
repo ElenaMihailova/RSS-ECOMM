c4d4a82ee719a08cbcfa83c9afafcee82d876e7f
Elena <pumpkinpum@gmail.com>
2023-09-14 23:01:16 +0300

    feat: add function

---
+++ src/modules/api/apiCart.ts
@@ -0,0 +1,28 @@
+import { Cart } from '@commercetools/platform-sdk';
+import { apiProjectRoot } from './buildRoot';
+
+const getCartById = async (cartId: string): Promise<Cart> => {
+  let resData: Cart | null = null;
+  try {
+    const response = await apiProjectRoot
+      .carts()
+      .get({
+        queryArgs: {
+          where: `id="${cartId}"`,
+        },
+      })
+      .execute();
+
+    [resData] = [...response.body.results];
+  } catch (err) {
+    console.error(err);
+  }
+
+  if (resData === null) {
+    throw new Error('Cart not found or another error occurred');
+  }
+
+  return resData;
+};
+
+export default getCartById;
